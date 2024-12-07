1a28ac82abce5e07d7c44cd9e7d6659124ec8bc7
Treee <itsatreee.twitch@gmail.com>
2024-12-07 16:08:01 -0700

    one of everything pretty much done

--- iat/addons/one_of_everything/scripts/3_game/iat_oneofeverythinghelper.c
+++ iat/addons/one_of_everything/scripts/3_game/iat_oneofeverythinghelper.c
@@ -1,28 +1,32 @@
 class IAT_OneOfEverythingConfig
 {
     protected vector m_StartingPoint;
-    protected vector m_SpacingBetweenObjects;
     protected int m_NumberObjectsPerRow;
     protected bool m_DebugMode;
+    protected bool m_Enabled;
 
-    void IAT_OneOfEverythingConfig(vector startingPoint, vector spacing, int numObjects, int debug)
+    void IAT_OneOfEverythingConfig(vector startingPoint, int numObjects, int debugMode, int enabled)
     {
         m_StartingPoint = startingPoint;
-        m_SpacingBetweenObjects = spacing;
         m_NumberObjectsPerRow = numObjects;
-        m_DebugMode = debug;
+        m_DebugMode = debugMode;
+        m_Enabled = enabled;
     }
     vector GetStartingPosition()
     {
         return m_StartingPoint;
     }
-    vector GetSpacingBetweenObjects()
-    {
-        return m_SpacingBetweenObjects;
-    }
     int GetNumberOfObjectsPerRow()
     {
         return m_NumberObjectsPerRow;
+    }
+    bool GetDebugMode()
+    {
+        return m_DebugMode;
+    }
+    bool GetIsEnabled()
+    {
+        return m_Enabled;
     }
 };
 
@@ -34,6 +38,12 @@ class IAT_OneOfEverythingHelper
     protected string m_RootModFolder = "ItsATreeMods";
     protected string m_JsonFile = "OneOfEverythingConfig.json";
     protected string m_P3dTextFile = "AllP3ds.txt";
+
+	protected vector m_ObjectRowPadding = "0 0 0";
+	protected vector m_LastRowStartingPosition = "0 0 0";
+
+    protected bool m_IsDebugMode = false;
+    protected bool m_IsEnabled = false;
 
     IAT_OneOfEverythingConfig GetOneOfEverythingConfig()
     {
@@ -50,7 +60,7 @@ class IAT_OneOfEverythingHelper
             // if the actual config file doesnt exist
             if (!FileExist(jsonConfig))
             {
-                iat_OOEConfig = new IAT_OneOfEverythingConfig("0 0 0", "1.5 0 0", 100, false);
+                iat_OOEConfig = new IAT_OneOfEverythingConfig("4000 500 4000", 100, false, false);
                 JsonFileLoader<ref IAT_OneOfEverythingConfig>.JsonSaveFile(jsonConfig, iat_OOEConfig);
             }
             else
@@ -67,13 +77,17 @@ class IAT_OneOfEverythingHelper
                 FPrintln(logFile, "rootFolder/path/to/someObject.p3d");
                 CloseFile(logFile);
             }
-
+            m_IsDebugMode = iat_OOEConfig.GetDebugMode();
+            m_IsEnabled = iat_OOEConfig.GetIsEnabled();
         }
         return iat_OOEConfig;
     }
 
     bool LoadAllP3ds()
     {
+        if (!m_IsEnabled)
+            return true;
+
         string rootFilePath = string.Format("%1\\%2", m_DayZFolder, m_RootModFolder);
         string p3dText = string.Format("%1\\%2", rootFilePath, m_P3dTextFile);
 
@@ -85,7 +99,7 @@ class IAT_OneOfEverythingHelper
 		int line_index = 1;
 
         vector p3dPosition = iat_OOEConfig.GetStartingPosition();
-        vector startingPointOffset = iat_OOEConfig.GetSpacingBetweenObjects();
+		m_LastRowStartingPosition = p3dPosition;
         int totalObjectsPerRowMax = iat_OOEConfig.GetNumberOfObjectsPerRow();
         int objectCounter = 1;
         int rowNumber = 0;
@@ -96,9 +110,13 @@ class IAT_OneOfEverythingHelper
             {
                 rowNumber++;
                 // move starting point back like a typewritter
-                p3dPosition = iat_OOEConfig.GetStartingPosition();
-                p3dPosition[2] = p3dPosition[2] + (rowNumber * 2);
+                p3dPosition = m_LastRowStartingPosition;
+				// add largest padding from the row of objects
+                p3dPosition[0] = p3dPosition[0] + (m_ObjectRowPadding[0] * 2);
+				// set last row to this rows starting point
+				m_LastRowStartingPosition = p3dPosition;
                 objectCounter = 0;
+				m_ObjectRowPadding = "0 0 0";
             }
             p3dPosition = SpawnSingleObject(p3dPosition, line_content);
 			char_count = FGets( m_FileHandle,  line_content );
@@ -138,11 +156,33 @@ class IAT_OneOfEverythingHelper
             min = "0 0 0";
             max = "1 1 1";
         }
-        PrintFormat("%1 has min: %2 max: %3", objectPath, min, max);
-        // send out the new position offset by this objects size + some spacer distance
+		float maxX = Math.Max(Math.AbsFloat(min[0]), Math.AbsFloat(max[0]));
+		float maxY = Math.Max(Math.AbsFloat(min[1]), Math.AbsFloat(max[1]));
+		float maxZ = Math.Max(Math.AbsFloat(min[2]), Math.AbsFloat(max[2])); // magic number to give some slack between objects
+		string absoluteBuffer = string.Format("%1 %2 %3", maxX, maxY, maxZ);
+		UpdateObjectRowPadding(absoluteBuffer.ToVector());
 
-        position = position + "1.5 0 0";
+        if (m_IsDebugMode)
+       	    PrintFormat("%1 has min: %2 max: %3 with absoluteBuffer of: %4", objectPath, min, max, absoluteBuffer);
+
+        // send out the new position offset by this objects size + some spacer distance
+        maxZ = Math.Max(6, maxZ * 2);
+		string itembuffer = string.Format("%1 %2 %3", 0, 0, maxZ);
+        position = position + itembuffer.ToVector();
         return position;
     }
+
+	void UpdateObjectRowPadding(vector newBoundaries)
+	{
+		// check if x or z is bigger. ignore y because it is irrelevant for this
+		if (m_ObjectRowPadding[0] < newBoundaries[0])
+		{
+			m_ObjectRowPadding[0] = newBoundaries[0];
+		}
+		if (m_ObjectRowPadding[2] < newBoundaries[2])
+		{
+			m_ObjectRowPadding[2] = newBoundaries[2];
+		}
+	}
 
 };
