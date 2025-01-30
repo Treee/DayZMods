d571ed4b728d7d2d98ec7093ed0a737131aeb672
Treee <itsatreee.twitch@gmail.com>
2025-01-30 11:34:13 -0700

    changing rpc to be on a single join instead of each respawn

--- iat_facepaints/addons/scripts/5_mission/mission/modded_missionserver.c
+++ iat_facepaints/addons/scripts/5_mission/mission/modded_missionserver.c
@@ -5,9 +5,9 @@ modded class MissionServer
 		GetDayZGame().SetIATFacePaintConfig(GetDayZGame().GetIATFacePaintConfig());
 	}
 
-	override void InvokeOnConnect(PlayerBase player, PlayerIdentity identity)
+	override void OnClientReadyEvent(PlayerIdentity identity, PlayerBase player)
 	{
-		super.InvokeOnConnect(player, identity);
+		super.OnClientReadyEvent(identity, player);
 
 		auto configParams = new Param1<IAT_FacePaintsConfig>(GetDayZGame().GetIATFacePaintConfig());
 		PrintFormat("Sending IAT_FacePaints Mod Config to Player: %1 RPC: %2", identity.GetName(), IAT_FACEPAINT_RPC.CHECK_FACEPAINT_CONFIG);
