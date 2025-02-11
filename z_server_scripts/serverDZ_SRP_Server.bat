4c26806c9f5aa4235b513f5ae7bbac47b0f68622
Treee <itsatreee.twitch@gmail.com>
2025-02-11 10:50:47 -0700

    1.2.1 server scripts update

--- z_server_scripts/serverDZ_SRP_Server.bat
+++ z_server_scripts/serverDZ_SRP_Server.bat
@@ -18,7 +18,7 @@ cd "%serverLocation%"
 echo (%time%) %serverName% started.
 
 @REM start is a special console command to invoke a start process (like an exe)
-start "DayZ Server" /min "DayZServer_x64.exe" -config=serverDZ_SRP.cfg -port=2302 "-profiles=profile_survivalists" -limitFPS=120 "-mod=Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@CF;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Survivor Animations;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@VPPAdminTools;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Bitterroot;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Code Lock;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@ViewInventoryAnimation;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@DayZ Horse;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Crocodile;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals Mutant Mod;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals Cybernetic Mod;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals Custom Weapons;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals Simple Snapping;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals GearStand Mod;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals Terminals Mod;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals Hunting Mod;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals Underwear Mod;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals Dark Sight Mod;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals NVG Mod;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals Cannibal Mod;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals World Radio Mod;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Ambient Animals Pack;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@VanillaPlusPlusMap;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Ear-Plugs;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@IAT_LiteraryDevices;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@IAT_FacePaints;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@IAT_Crafting_Plus_Enums;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@IAT_Crafting_Plus;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@IAT_Enhanced_BioProtection;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@IAT_NotesEnhanced;P:\Mods\@IAT_WeaponQuickbar_Release;P:\Mods\@IAT_SimpleTerritories_Release;P:\Mods\@IAT_Structures_Release;P:\Mods\@IAT_MapBorderTeleport_Release;P:\Mods\@Survivalists RP Server Modpack;P:\Mods\@IAT_Dev_Release;" -scrAllowFileWrite -noBenchmark -scriptDebug=true -filePatching -doLogs -adminlog
+start "DayZ Server" /min "DayZServer_x64.exe" -config=serverDZ_SRP.cfg -port=2302 "-profiles=profile_survivalists" -limitFPS=120 "-mod=Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@CF;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Survivor Animations;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@VPPAdminTools;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Bitterroot;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Code Lock;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@ViewInventoryAnimation;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@DayZ Horse;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Crocodile;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals Dark Sight Mod;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals NVG Mod;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals World Radio Mod;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals Cannibal Mod;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals Simple Snapping;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals Underwear Mod;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals Hunting Mod;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals Terminals Mod;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals Mutant Mod;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals GearStand Mod;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals Cybernetic Mod;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals Custom Weapons;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Ambient Animals Pack;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@VanillaPlusPlusMap;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Ear-Plugs;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@IAT_LiteraryDevices;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@IAT_FacePaints;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@IAT_Crafting_Plus;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@IAT_Crafting_Plus_Enums;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@IAT_Enhanced_BioProtection;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@IAT_Corrosive_BioGas;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@IAT_Weapon_Quickbar_Realism;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@IAT_NotesEnhanced;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@IAT_Structures;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@IAT_Simple_Territories;P:\@Survivalists RP Server Modpack;" -scrAllowFileWrite -noBenchmark -scriptDebug=true -filePatching -doLogs -adminlog
 
 @REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@CF;
 @REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Survivor Animations;
@@ -28,28 +28,31 @@ start "DayZ Server" /min "DayZServer_x64.exe" -config=serverDZ_SRP.cfg -port=230
 @REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@ViewInventoryAnimation;
 @REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@DayZ Horse;
 @REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Crocodile;
-@REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals Mutant Mod;
-@REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals Cybernetic Mod;
-@REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals Custom Weapons;
-@REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals Simple Snapping;
-@REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals GearStand Mod;
-@REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals Terminals Mod;
-@REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals Hunting Mod;
-@REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals Underwear Mod;
 @REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals Dark Sight Mod;
 @REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals NVG Mod;
-@REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals Cannibal Mod;
 @REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals World Radio Mod;
+@REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals Cannibal Mod;
+@REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals Simple Snapping;
+@REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals Underwear Mod;
+@REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals Hunting Mod;
+@REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals Terminals Mod;
+@REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals Mutant Mod;
+@REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals GearStand Mod;
+@REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals Cybernetic Mod;
+@REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals Custom Weapons;
 @REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Ambient Animals Pack;
 @REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@VanillaPlusPlusMap;
-@REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Ear-Plugs;
+@REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Ear-Plugs
 @REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@IAT_LiteraryDevices;
 @REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@IAT_FacePaints;
-@REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@IAT_Crafting_Plus_Enums;
 @REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@IAT_Crafting_Plus;
+@REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@IAT_Crafting_Plus_Enums;
 @REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@IAT_Enhanced_BioProtection;
+@REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@IAT_Corrosive_BioGas;
 @REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@IAT_Weapon_Quickbar_Realism;
 @REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@IAT_NotesEnhanced;
+@REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@IAT_Structures;
+@REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@IAT_Simple_Territories;
 @REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Survivalists RP Server Modpack;
 @REM P:\Mods\@Survivalists RP Server Modpack;
 @REM P:\Mods\@SRP_Tree_Mods;
