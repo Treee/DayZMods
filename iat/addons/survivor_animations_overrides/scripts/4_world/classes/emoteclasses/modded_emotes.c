cc9c44d6c2699b9e66d8f1a116bb6a24ea8fbfae
Treee <itsatreee.twitch@gmail.com>
2025-02-14 15:33:23 -0700

    removing certain emotes

---
+++ iat/addons/survivor_animations_overrides/scripts/4_world/classes/emoteclasses/modded_emotes.c
@@ -0,0 +1,31 @@
+modded class EmoteSurAnmScratchBalls
+{
+	override bool EmoteCondition(int stancemask)
+	{
+		return false;
+	}
+};
+
+modded class EmoteSurAnmScratchAss
+{
+	override bool EmoteCondition(int stancemask)
+	{
+		return false;
+	}
+};
+
+modded class EmoteSurAnmPoplacaniPoZadku
+{
+	override bool EmoteCondition(int stancemask)
+	{
+		return false;
+	}
+};
+
+modded class EmoteSurAnmPsouk
+{
+	override bool EmoteCondition(int stancemask)
+	{
+		return false;
+	}
+};
--- iat/release_version.hpp
+++ iat/release_version.hpp
@@ -1,4 +1,4 @@
 #define MAJOR 0
-#define MINOR 142
+#define MINOR 143
 #define PATCH 14
-#define BUILD 120
+#define BUILD 121
