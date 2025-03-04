29cc02a89e175740efaa2485a313c64b6fe425f8
Treee <itsatreee.twitch@gmail.com>
2025-03-04 12:22:49 -0700

    adding initial commit for roleplay emotes

--- link_to_p.bat
+++ link_to_p.bat
@@ -7,6 +7,7 @@ mklink /J "C:\Users\Tree\Documents\DayZ Projects\iat_literary_devices" Z:\DayZ\M
 mklink /J "C:\Users\Tree\Documents\DayZ Projects\iat_map_border_teleport" Z:\DayZ\Modding\iat_map_border_teleport\addons
 mklink /J "C:\Users\Tree\Documents\DayZ Projects\iat_notes_enhanced" Z:\DayZ\Modding\iat_notes_enhanced\addons
 mklink /J "C:\Users\Tree\Documents\DayZ Projects\iat_retextures" Z:\DayZ\Modding\iat_retextures\addons
+mklink /J "C:\Users\Tree\Documents\DayZ Projects\iat_roleplay_emotes" Z:\DayZ\Modding\iat_roleplay_emotes\addons
 mklink /J "C:\Users\Tree\Documents\DayZ Projects\iat_simple_territories" Z:\DayZ\Modding\iat_simple_territories\addons
 mklink /J "C:\Users\Tree\Documents\DayZ Projects\iat_structures" Z:\DayZ\Modding\iat_structures\addons
 mklink /J "C:\Users\Tree\Documents\DayZ Projects\iat_weapon_quickbar" Z:\DayZ\Modding\iat_weapon_quickbar\addons
