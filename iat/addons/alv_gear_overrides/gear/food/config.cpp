c5160d7688134e70166a29969d7177498dfe226f
Treee <itsatreee.twitch@gmail.com>
2025-04-14 10:13:50 -0600

    adding heavy item behavior to alevaric carcass

--- iat/addons/alv_gear_overrides/gear/food/config.cpp
+++ iat/addons/alv_gear_overrides/gear/food/config.cpp
@@ -12,30 +12,37 @@ class CfgVehicles
 	//==================================================== VANILLA OVERRIDE
 	class ALV_Carcass_Deer : ALV_Carcass_Base
 	{
+		itemBehaviour = 0;
 		itemSize[] = { 12, 12 };
 	};
 	class ALV_Carcass_Pig : ALV_Carcass_Base
 	{
+		itemBehaviour = 0;
 		itemSize[] = { 12, 12 };
 	};
 	class ALV_Carcass_Sheep : ALV_Carcass_Base
 	{
+		itemBehaviour = 0;
 		itemSize[] = { 12, 12 };
 	};
 	class ALV_Carcass_Goat : ALV_Carcass_Base
 	{
+		itemBehaviour = 0;
 		itemSize[] = { 12, 12 };
 	};
 	class ALV_Carcass_Wolf : ALV_Carcass_Base
 	{
+		itemBehaviour = 0;
 		itemSize[] = { 12, 12 };
 	};
 	class ALV_Carcass_Cow : ALV_Carcass_Base
 	{
+		itemBehaviour = 0;
 		itemSize[] = { 12, 12 };
 	};
 	class ALV_Carcass_Bear : ALV_Carcass_Base
 	{
+		itemBehaviour = 0;
 		itemSize[] = { 12, 12 };
 	};
 };
