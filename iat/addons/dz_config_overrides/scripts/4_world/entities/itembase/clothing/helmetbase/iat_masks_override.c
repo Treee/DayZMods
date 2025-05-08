5d5085319ac0043792060271c86726f8895b7fc5
Treee <itsatreee.twitch@gmail.com>
2025-05-08 09:00:43 -0600

    removing gas mask muffle

--- iat/addons/dz_config_overrides/scripts/4_world/entities/itembase/clothing/helmetbase/iat_masks_override.c
+++ iat/addons/dz_config_overrides/scripts/4_world/entities/itembase/clothing/helmetbase/iat_masks_override.c
@@ -1,72 +1,72 @@
-// modded class GasMask
-// {
-// 	override bool IsObstructingVoice()
-// 	{
-// 		return false;
-// 	}
+modded class GasMask
+{
+	override bool IsObstructingVoice()
+	{
+		return false;
+	}
 
-// 	override int GetVoiceEffect()
-// 	{
-// 		return 0;
-// 	}
-// };
-// modded class GP5GasMask
-// {
-// 	override bool IsObstructingVoice()
-// 	{
-// 		return false;
-// 	}
+	override int GetVoiceEffect()
+	{
+		return 0;
+	}
+};
+modded class GP5GasMask
+{
+	override bool IsObstructingVoice()
+	{
+		return false;
+	}
 
-// 	override int GetVoiceEffect()
-// 	{
-// 		return 0;
-// 	}
-// };
-// modded class GreatHelm
-// {
-// 	override bool IsObstructingVoice()
-// 	{
-// 		return false;
-// 	}
+	override int GetVoiceEffect()
+	{
+		return 0;
+	}
+};
+modded class GreatHelm
+{
+	override bool IsObstructingVoice()
+	{
+		return false;
+	}
 
-// 	override int GetVoiceEffect()
-// 	{
-// 		return 0;
-// 	}
-// };
-// modded class DarkMotoHelmet_ColorBase
-// {
-// 	override bool IsObstructingVoice()
-// 	{
-// 		return false;
-// 	}
+	override int GetVoiceEffect()
+	{
+		return 0;
+	}
+};
+modded class DarkMotoHelmet_ColorBase
+{
+	override bool IsObstructingVoice()
+	{
+		return false;
+	}
 
-// 	override int GetVoiceEffect()
-// 	{
-// 		return 0;
-// 	}
-// };
-// modded class MotoHelmet_ColorBase
-// {
-// 	override bool IsObstructingVoice()
-// 	{
-// 		return false;
-// 	}
+	override int GetVoiceEffect()
+	{
+		return 0;
+	}
+};
+modded class MotoHelmet_ColorBase
+{
+	override bool IsObstructingVoice()
+	{
+		return false;
+	}
 
-// 	override int GetVoiceEffect()
-// 	{
-// 		return 0;
-// 	}
-// };
-// modded class AirborneMask
-// {
-// 	override bool IsObstructingVoice()
-// 	{
-// 		return false;
-// 	}
+	override int GetVoiceEffect()
+	{
+		return 0;
+	}
+};
+modded class AirborneMask
+{
+	override bool IsObstructingVoice()
+	{
+		return false;
+	}
 
-// 	override int GetVoiceEffect()
-// 	{
-// 		return 0;
-// 	}
-// };
+	override int GetVoiceEffect()
+	{
+		return 0;
+	}
+};
--- iat/release_version.hpp
+++ iat/release_version.hpp
@@ -1,4 +1,4 @@
 #define MAJOR 0
-#define MINOR 150
-#define PATCH 19
-#define BUILD 153
+#define MINOR 151
+#define PATCH 20
+#define BUILD 154
