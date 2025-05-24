50defe340f7ac11d3b4c759bdf257c938c2295b3
Treee <itsatreee.twitch@gmail.com>
2025-05-23 21:20:06 -0600

    adding snailien shovel

--- iat_prop_items/addons/scripts/4_world/modded_itemregistered.c
+++ iat_prop_items/addons/scripts/4_world/modded_itemregistered.c
@@ -4,21 +4,21 @@ modded class ModItemRegisterCallbacks
     {
         super.RegisterOneHanded( pType, pBehavior );
         pType.AddItemInHandsProfileIK("IAT_Bear_ColorBase", "dz/anims/workspaces/player/player_main/player_main_1h.asi", pBehavior, "dz/anims/anm/player/ik/gear/TeddyBear.anm");
+    }
 
-    };
+    override void RegisterTwoHanded(DayZPlayerType pType, DayzPlayerItemBehaviorCfg pBehavior)
+    {
+        super.RegisterTwoHanded( pType, pBehavior );
 
-    // override void RegisterTwoHanded(DayZPlayerType pType, DayzPlayerItemBehaviorCfg pBehavior)
-    // {
-    //     super.RegisterTwoHanded( pType, pBehavior );
+        //====================================================
+        pType.AddItemInHandsProfileIK("IAT_Shovel_Colorbase", "dz/anims/workspaces/player/player_main/weapons/player_main_2h_shovel.asi", pBehavior, "dz/anims/anm/player/ik/two_handed/Shovel.anm");
+    }
 
-    //     //==================================================== BLADE WEAPONS
-    //     pType.AddItemInHandsProfileIK("IAT_Gramaphone_Colorbase", "dz/anims/workspaces/player/player_main/weapons/player_main_2h_sword.asi", pBehavior,	"dz/anims/anm/player/ik/two_handed/medieval_sword.anm");
-    // }
     override void RegisterHeavy(DayZPlayerType pType, DayzPlayerItemBehaviorCfg pBehavior)
     {
         super.RegisterHeavy( pType, pBehavior );
 
-        //==================================================== BLADE WEAPONS
+        //====================================================
         pType.AddItemInHandsProfileIK("IAT_Gramaphone_Colorbase", "dz/anims/workspaces/player/player_main/player_main_heavy.asi", pBehavior, "dz/anims/anm/player/ik/heavy/sea_chest.anm");
     }
 };
