4cab0e99a3b7bfc99720a0c3b84a95a7ead9c7b5
Treee <itsatreee.twitch@gmail.com>
2025-05-26 11:31:08 -0600

    adding thunderhammer

--- iat_prop_items/addons/tools/config.cpp
+++ iat_prop_items/addons/tools/config.cpp
@@ -138,4 +138,116 @@ class CfgVehicles
         hiddenSelections[] = { "zbytek" };
         hiddenSelectionsTextures[] = { "iat_prop_items\tools\data\iat_shovel_co.paa" };
     };
+
+    class IAT_IronHammer_Colorbase : Inventory_Base
+    {
+        scope = 0;
+        debug_ItemCategory = 2;
+        displayName = "Iron Hammer";
+        descriptionShort = "A strong iron hammer that can be used in place of a sledgehammer.";
+        model = "iat_prop_items\tools\iat_ironhammer.p3d";
+        itemInfo[] = { "SledgeHammer" };
+        rotationFlags = 12;
+        weight = 5000;
+        itemSize[] = { 2, 6 };
+        itemBehaviour = 2;
+        openItemSpillRange[] = { 30, 50 };
+        inventorySlot[] = { "Shoulder", "Melee" };
+        isMeleeWeapon = 1;
+        build_action_type = 16;
+        dismantle_action_type = 16;
+        soundImpactType = "metal";
+        hiddenSelections[] = { "zbytek" };
+        hiddenSelectionsTextures[] = { "iat_prop_items\tools\data\iat_thunderhammer_co.paa" };
+        class DamageSystem
+        {
+            class GlobalHealth
+            {
+                class Health
+                {
+                    hitpoints = 300;
+                    healthLevels[] =
+                    {
+                        {1, {"iat_prop_items\tools\data\iat_thunderhammer.rvmat"}},
+                        {0.7, {"iat_prop_items\tools\data\iat_thunderhammer.rvmat"}},
+                        {0.5, {"iat_prop_items\tools\data\iat_thunderhammer_damage.rvmat"}},
+                        {0.3, {"iat_prop_items\tools\data\iat_thunderhammer_damage.rvmat"}},
+                        {0, {"iat_prop_items\tools\data\iat_thunderhammer_destruct.rvmat"}}
+                    };
+                };
+            };
+        };
+        class MeleeModes
+        {
+            class Default
+            {
+                ammo = "MeleeBluntLight_5";
+                range = 1.8;
+            };
+            class Heavy
+            {
+                ammo = "MeleeBluntHeavy_5";
+                range = 1.8;
+            };
+            class Sprint
+            {
+                ammo = "MeleeBluntHeavy_5";
+                range = 3.7;
+            };
+        };
+        class AnimEvents
+        {
+            class SoundWeapon
+            {
+                class pickup_light
+                {
+                    soundSet = "hatchet_pickup_light_SoundSet";
+                    id = 796;
+                };
+                class pickup
+                {
+                    soundSet = "hatchet_pickup_SoundSet";
+                    id = 797;
+                };
+                class drop
+                {
+                    soundset = "woodaxe_drop_SoundSet";
+                    id = 898;
+                };
+                class SledgeWoodHammer_loop
+                {
+                    soundSet = "SledgeWoodHammer_loop_SoundSet";
+                    id = 1117;
+                };
+                class SledgeWoodHammer_end
+                {
+                    soundSet = "SledgeWoodHammer_end_SoundSet";
+                    id = 1118;
+                };
+                class ShoulderR_Hide
+                {
+                    soundset = "ShoulderR_Hide_SoundSet";
+                    id = 1210;
+                };
+                class ShoulderR_Show
+                {
+                    soundset = "ShoulderR_Show_SoundSet";
+                    id = 1211;
+                };
+            };
+        };
+    };
+
+    class IAT_IronHammer_Basic : IAT_IronHammer_Colorbase
+    {
+        scope = 2;
+        hiddenSelections[] = { "zbytek" };
+        hiddenSelectionsTextures[] = { "iat_prop_items\tools\data\iat_thunderhammer_co.paa" };
+    };
+    class IAT_IronHammer_jaygarrick : IAT_IronHammer_Colorbase
+    {
+        scope = 2;
+        hiddenSelections[] = { "zbytek" };
+        hiddenSelectionsTextures[] = { "iat_prop_items\tools\data\iat_thunderhammer_jaygarrick_co.paa" };
+    };
 };
