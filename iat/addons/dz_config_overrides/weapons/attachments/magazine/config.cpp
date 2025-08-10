f061034e515299797024d770903d103b4015a58f
Treee <itsatreee.twitch@gmail.com>
2025-08-10 17:19:50 -0600

    fixing mag names

---
+++ iat/addons/dz_config_overrides/weapons/attachments/magazine/config.cpp
@@ -0,0 +1,22 @@
+class CfgPatches
+{
+	class IAT_DZ_ConfigOverrides_Weapons_Magazines
+	{
+		requiredAddons[] =
+		{
+			"DZ_Weapons_Magazines"
+		};
+	};
+};
+class CfgMagazines
+{
+	class Mag_CMAG_40Rnd;
+	class Mag_CMAG_40Rnd_Black : Mag_CMAG_40Rnd
+	{
+		descriptionShort = "$STR_CfgMagazines_Mag_CMAG_40Rnd1";
+	};
+	class Mag_CMAG_40Rnd_Green : Mag_CMAG_40Rnd
+	{
+		descriptionShort = "$STR_CfgMagazines_Mag_CMAG_40Rnd1";
+	};
+};
