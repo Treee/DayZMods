c38b5dd31160de19f8a28f8ef63570b6c3dbd93f
Treee <itsatreee.twitch@gmail.com>
2025-08-15 14:05:06 -0600

    adding new texture

--- iat_melee_weapons/release_version.hpp
+++ iat_melee_weapons/release_version.hpp
@@ -1,4 +1,4 @@
 #define MAJOR 0
 #define MINOR 4
 #define PATCH 8
-#define BUILD 54
+#define BUILD 55
--- iat_mining_enhanced/addons/scripts/4_world/classes/modded_hologram.c
+++ iat_mining_enhanced/addons/scripts/4_world/classes/modded_hologram.c
@@ -6,25 +6,18 @@ modded class Hologram
 	{
 		if (m_ProjectionTypename == "land_iat_miningsegment_entrance")
 		{
-			position[0] = Math.Round(position[0]);
-			position[1] = Math.Round(position[1]);
-			position[2] = Math.Round(position[2]);
-			// vector clamped = position;
-
 			// // 8 is the default junction space so clamp it by that
-			// clamped[0] = clamped[0] / 8;
-			// clamped[1] = clamped[1] / 8;
-			// clamped[2] = clamped[2] / 8;
+			position[0] = position[0] / 8;
+			// position[1] = position[1] / 8;
+			position[2] = position[2] / 8;
 
 			// // remove any decimals
-			// clamped = MiscGameplayFunctions.TruncateVec(clamped, 0);
+			position = MiscGameplayFunctions.TruncateVec(position, 0);
 
-			// // rescale back to best position
-			// clamped[0] = clamped[0] * 8;
-			// clamped[1] = clamped[1] * 8;
-			// clamped[2] = clamped[2] * 8;
-
-			// position = clamped;
+			// rescale back to best position
+			position[0] = position[0] * 8;
+			// position[1] = position[1] * 8;
+			position[2] = position[2] * 8;
 		}
 		super.SetProjectionPosition(position);
 	}
