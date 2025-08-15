e26ce43fb912452dd4881111bdeb34c22492679e
Treee <itsatreee.twitch@gmail.com>
2025-08-15 13:25:17 -0600

    adding damascus bowie knife

--- iat_melee_weapons/addons/blade/config.cpp
+++ iat_melee_weapons/addons/blade/config.cpp
@@ -1875,8 +1875,270 @@ class CfgVehicles
     class IAT_Narsil_Obamamama2580 : IAT_Narsil_Colorbase
     {
         scope = 2;
+        TradeClassName = "HuntingKnife";
         hiddenSelections[] = { "zbytek" };
         hiddenSelectionsTextures[] = { "iat_melee_weapons\blade\data\iat_narsilblade_obamamama2580_co.paa" };
+    };
+
+    class IAT_DamascusBowieKnife_Colorbase : Inventory_Base
+    {
+        scope = 0;
+        displayName = "Damascus Bowie Knife";
+        descriptionShort = "A well forged damascus steal blade.";
+        model = "iat_melee_weapons\blade\iat_damascusbowie.p3d";
+        debug_ItemCategory = 2;
+        inventorySlot[] = { "Knife" };
+        repairableWithKits[] = { 4 };
+        repairCosts[] = { 10 };
+        rotationFlags = 17;
+        RestrainUnlockType = 1;
+        canSkinBodies = 1;
+        weight = 200;
+        itemSize[] = { 1, 4 };
+        openItemSpillRange[] = { 10, 20 };
+        itemInfo[] = { "Knife" };
+        hiddenSelections[] = { "zbytek", "handle" };
+        hiddenSelectionsTextures[] =
+        {
+            "iat_melee_weapons\blade\data\iat_damascusbowie_co.paa",
+            "iat_melee_weapons\blade\data\iat_damascusbowie_co.paa",
+        };
+        hiddenSelectionsMaterials[] =
+        {
+            "iat_melee_weapons\blade\data\iat_damascusbowie.rvmat",
+            "iat_melee_weapons\blade\data\iat_damascusbowiewood.rvmat",
+        };
+        class DamageSystem
+        {
+            class GlobalHealth
+            {
+                class Health
+                {
+                    hitpoints = 130;
+                    healthLevels[] =
+                    {
+                        {1, {"iat_melee_weapons\blade\data\iat_damascusbowie.rvmat"}},
+                        {0.7, {"iat_melee_weapons\blade\data\iat_damascusbowie.rvmat"}},
+                        {0.5, {"iat_melee_weapons\blade\data\iat_damascusbowie_damage.rvmat"}},
+                        {0.3, {"iat_melee_weapons\blade\data\iat_damascusbowie_damage.rvmat"}},
+                        {0, {"iat_melee_weapons\blade\data\iat_damascusbowie_destruct.rvmat"}}
+                    };
+                };
+            };
+        };
+        isMeleeWeapon = 1;
+        suicideAnim = "onehanded";
+        class MeleeModes
+        {
+            class Default
+            {
+                ammo = "MeleeSharpLight_3";
+                range = 1.4;
+            };
+            class Heavy
+            {
+                ammo = "MeleeSharpHeavy_3";
+                range = 1.4;
+            };
+            class Sprint
+            {
+                ammo = "MeleeSharpHeavy_3";
+                range = 3.3;
+            };
+        };
+        soundImpactType = "metal";
+        class AnimEvents
+        {
+            class SoundWeapon
+            {
+                class animalSkinning_in
+                {
+                    soundSet = "animalSkinning_in_SoundSet";
+                    id = 516;
+                };
+                class animalSkinning
+                {
+                    soundSet = "animalSkinning_SoundSet";
+                    id = 517;
+                };
+                class animalSkinning_out
+                {
+                    soundSet = "animalSkinning_out_SoundSet";
+                    id = 518;
+                };
+                class CutTies_end
+                {
+                    soundSet = "CutTies_end_SoundSet";
+                    id = 519;
+                };
+                class CutTies_loop
+                {
+                    soundSet = "CutTies_loop_SoundSet";
+                    id = 520;
+                };
+                class shaving_knife_clean
+                {
+                    soundSet = "shaving_knife_clean_SoundSet";
+                    id = 600;
+                };
+                class shaving_knife1
+                {
+                    soundSet = "shaving_knife1_SoundSet";
+                    id = 601;
+                };
+                class shaving_knife2
+                {
+                    soundSet = "shaving_knife2_SoundSet";
+                    id = 602;
+                };
+                class shaving_knife3
+                {
+                    soundSet = "shaving_knife3_SoundSet";
+                    id = 603;
+                };
+                class shaving_knife4
+                {
+                    soundSet = "shaving_knife4_SoundSet";
+                    id = 604;
+                };
+                class shaving_knife5
+                {
+                    soundSet = "shaving_knife5_SoundSet";
+                    id = 605;
+                };
+                class shaving_knife6
+                {
+                    soundSet = "shaving_knife6_SoundSet";
+                    id = 606;
+                };
+                class shaving_knife7
+                {
+                    soundSet = "shaving_knife7_SoundSet";
+                    id = 607;
+                };
+                class shaving_knife8
+                {
+                    soundSet = "shaving_knife8_SoundSet";
+                    id = 608;
+                };
+                class shaving_knife9
+                {
+                    soundSet = "shaving_knife9_SoundSet";
+                    id = 609;
+                };
+                class shaving_knife10
+                {
+                    soundSet = "shaving_knife10_SoundSet";
+                    id = 610;
+                };
+                class shaving_knife11
+                {
+                    soundSet = "shaving_knife11_SoundSet";
+                    id = 611;
+                };
+                class shaving_knife12
+                {
+                    soundSet = "shaving_knife12_SoundSet";
+                    id = 612;
+                };
+                class shaving_knife13
+                {
+                    soundSet = "shaving_knife13_SoundSet";
+                    id = 613;
+                };
+                class shaving_knife14
+                {
+                    soundSet = "shaving_knife14_SoundSet";
+                    id = 614;
+                };
+                class shaving_knife15
+                {
+                    soundSet = "shaving_knife15_SoundSet";
+                    id = 615;
+                };
+                class shaving_knife16
+                {
+                    soundSet = "shaving_knife16_SoundSet";
+                    id = 616;
+                };
+                class shaving_knife17
+                {
+                    soundSet = "shaving_knife17_SoundSet";
+                    id = 617;
+                };
+                class shaving_knife18
+                {
+                    soundSet = "shaving_knife18_SoundSet";
+                    id = 618;
+                };
+                class shaving_knife19
+                {
+                    soundSet = "shaving_knife19_SoundSet";
+                    id = 619;
+                };
+                class shaving_knife20
+                {
+                    soundSet = "shaving_knife20_SoundSet";
+                    id = 620;
+                };
+                class shaving_knife21
+                {
+                    soundSet = "shaving_knife21_SoundSet";
+                    id = 621;
+                };
+                class pickup_light
+                {
+                    soundSet = "hatchet_pickup_light_SoundSet";
+                    id = 796;
+                };
+                class pickup
+                {
+                    soundSet = "hatchet_pickup_SoundSet";
+                    id = 797;
+                };
+                class drop
+                {
+                    soundset = "woodaxe_drop_SoundSet";
+                    id = 898;
+                };
+                class FirefighterAxe_loop_SoundSet
+                {
+                    soundSet = "FirefighterAxe_loop_SoundSet";
+                    id = 1121;
+                };
+                class FirefighterAxe_end_SoundSet
+                {
+                    soundSet = "FirefighterAxe_end_SoundSet";
+                    id = 1122;
+                };
+            };
+            class Sound
+            {
+                class SurfaceDig
+                {
+                    class DigPrimary
+                    {
+                        default = 906;
+                        sakhal_snow = 906;
+                        sakhal_snow_forest = 906;
+                        sakhal_grass_brown = 906;
+                    };
+                };
+            };
+        };
+    };
+    class IAT_DamascusBowieKnife_Basic : IAT_DamascusBowieKnife_Colorbase
+    {
+        scope = 2;
+        hiddenSelections[] = { "zbytek" };
+        hiddenSelectionsTextures[] = { "iat_melee_weapons\blade\data\iat_damascusbowie_co.paa" };
+    };
+    class IAT_DamascusBowieKnife_sir_bman : IAT_DamascusBowieKnife_Colorbase
+    {
+        scope = 2;
+        TradeClassName = "HuntingKnife";
+        hiddenSelections[] = { "zbytek" };
+        hiddenSelectionsTextures[] = { "iat_melee_weapons\blade\data\iat_damascusbowie_sir_bman_co.paa" };
     };
 
     class IAT_Katana_ColorBase : Inventory_Base
