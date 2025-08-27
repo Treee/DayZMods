48779cdde8f27db2f37da7fd61e5cc971e06b984
Treee <itsatreee.twitch@gmail.com>
2025-08-27 15:57:58 -0600

    adding reaver mask

--- iat_prop_items/addons/characters/mask/config.cpp
+++ iat_prop_items/addons/characters/mask/config.cpp
@@ -77,4 +77,75 @@ class CfgVehicles
         hiddenSelections[] = { "zbytek" };
         hiddenSelectionsTextures[] = { "iat_prop_items\characters\mask\data\iat_skullpredatormask_rogueknight1995_co.paa" };
     };
+
+
+    class IAT_ReaverMask_Colorbase : Clothing
+    {
+        scope = 0;
+        displayName = "Reaver Mask";
+        descriptionShort = "A mask with holes drilled through the metal.";
+        model = "iat_prop_items\characters\mask\iat_reavermask_g.p3d";
+        inventorySlot[] = { "Mask" };
+        itemInfo[] = { "Clothing", "Mask" };
+        weight = 700;
+        itemSize[] = { 2, 2 };
+        varWetMax = 0.2;
+        heatIsolation = 0.1;
+        repairableWithKits[] = { 5, 2 };
+        repairCosts[] = { 30, 25 };
+        hiddenSelections[] = { "zbytek" };
+        hiddenSelectionsTextures[] = { "iat_prop_items\characters\mask\data\iat_reavermask_co.paa" };
+        class DamageSystem
+        {
+            class GlobalHealth
+            {
+                class Health
+                {
+                    hitpoints = 100;
+                    healthLevels[] =
+                    {
+                        {1, {"iat_prop_items\characters\mask\data\iat_reavermask.rvmat"}},
+                        {0.7, {"iat_prop_items\characters\mask\data\iat_reavermask.rvmat"}},
+                        {0.5, {"iat_prop_items\characters\mask\data\iat_reavermask_damage.rvmat"}},
+                        {0.3, {"iat_prop_items\characters\mask\data\iat_reavermask_damage.rvmat"}},
+                        {0, {"iat_prop_items\characters\mask\data\iat_reavermask_destruct.rvmat"}}
+                    };
+                };
+            };
+        };
+        class ClothingTypes
+        {
+            male = "iat_prop_items\characters\mask\iat_reavermask_m.p3d";
+            female = "iat_prop_items\characters\mask\iat_reavermask_m.p3d";
+        };
+        class AnimEvents
+        {
+            class SoundWeapon
+            {
+                class pickUpItem
+                {
+                    soundSet = "Shirt_pickup_SoundSet";
+                    id = 797;
+                };
+                class drop
+                {
+                    soundset = "Shirt_drop_SoundSet";
+                    id = 898;
+                };
+            };
+        };
+    };
+    class IAT_ReaverMask_Basic : IAT_ReaverMask_Colorbase
+    {
+        scope = 2;
+        hiddenSelections[] = { "zbytek" };
+        hiddenSelectionsTextures[] = { "iat_prop_items\characters\mask\data\iat_reavermask_co.paa" };
+    };
+    class IAT_ReaverMask_eternallyfree : IAT_ReaverMask_Colorbase
+    {
+        scope = 2;
+        TradeClassName = "MetalPlate";
+        hiddenSelections[] = { "zbytek" };
+        hiddenSelectionsTextures[] = { "iat_prop_items\characters\mask\data\iat_reavermask_eternalfyree_co.paa" };
+    };
 };
---
+++ iat_prop_items/addons/scripts/4_world/entities/itembase/clothing/masks/iat_reavermask.c
@@ -0,0 +1,4 @@
+class IAT_ReaverMask_Colorbase extends Mask_Base{};
+
+class IAT_ReaverMask_Basic extends IAT_ReaverMask_Colorbase{};
+class IAT_ReaverMask_eternallyfree extends IAT_ReaverMask_Colorbase{};
