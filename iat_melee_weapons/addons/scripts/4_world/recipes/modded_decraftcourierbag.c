3b882e8dcb197572e69ee99679f0f50c059c25c9
Treee <itsatreee.twitch@gmail.com>
2025-09-02 12:51:36 -0600

    adding knife recipes for custom knifes

---
+++ iat_melee_weapons/addons/scripts/4_world/recipes/modded_decraftcourierbag.c
@@ -0,0 +1,12 @@
+modded class DeCraftCourierBag
+{
+	override void Init()
+	{
+		// make sure existing behavior is set up
+		super.Init();
+
+		// multi tool is like can opener
+		InsertIngredient(1,"IAT_Narsil_Colorbase");
+		InsertIngredient(1,"IAT_DamascusBowieKnife_Colorbase");
+	}
+};
