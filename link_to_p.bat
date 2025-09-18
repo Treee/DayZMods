c403ad3d2fb81682f05211726dc657b1a197302a
Treee <itsatreee.twitch@gmail.com>
2025-09-18 15:10:03 -0600

    adding project stub

--- link_to_p.bat
+++ link_to_p.bat
@@ -1,5 +1,6 @@
 mklink /J "C:\Users\Tree\Documents\DayZ Projects\iat" Z:\DayZ\Modding\iat\addons
 mklink /J "C:\Users\Tree\Documents\DayZ Projects\iat_codelock" Z:\DayZ\Modding\iat_codelock\addons
+mklink /J "C:\Users\Tree\Documents\DayZ Projects\iat_collectibles" Z:\DayZ\Modding\iat_collectibles\addons
 mklink /J "C:\Users\Tree\Documents\DayZ Projects\iat_corrosive_biogas" Z:\DayZ\Modding\iat_corrosive_biogas\addons
 mklink /J "C:\Users\Tree\Documents\DayZ Projects\iat_crafting_plus" Z:\DayZ\Modding\iat_crafting_plus\addons
 mklink /J "C:\Users\Tree\Documents\DayZ Projects\iat_dynamic_radio_range" Z:\DayZ\Modding\iat_dynamic_radio_range\addons
