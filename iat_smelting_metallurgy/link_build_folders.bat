495a0e37ba945b0b3681bd36297a83810ee3d411
Treee <itsatreee.twitch@gmail.com>
2025-11-05 12:58:52 -0700

    fixing folder name typo

--- iat_smelting_metallurgy/link_build_folders.bat
+++ iat_smelting_metallurgy/link_build_folders.bat
@@ -1,2 +1,2 @@
-mklink /J "C:\Users\Tree\Documents\DayZ Projects\Mods\@IAT_SmeltingMettallurgy_Dev" Z:\DayZ\Modding\iat_smelting_metallurgy\.hemttout\dev
-mklink /J "C:\Users\Tree\Documents\DayZ Projects\Mods\@IAT_SmeltingMettallurgy_Release" Z:\DayZ\Modding\iat_smelting_metallurgy\.hemttout\release
+mklink /J "C:\Users\Tree\Documents\DayZ Projects\Mods\@IAT_SmeltingMetallurgy_Dev" Z:\DayZ\Modding\iat_smelting_metallurgy\.hemttout\dev
+mklink /J "C:\Users\Tree\Documents\DayZ Projects\Mods\@IAT_SmeltingMetallurgy_Release" Z:\DayZ\Modding\iat_smelting_metallurgy\.hemttout\release
