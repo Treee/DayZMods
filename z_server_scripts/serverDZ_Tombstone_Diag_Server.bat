e5010286bad11277f692d2718b1968f30ecf27e8
Treee <itsatreee.twitch@gmail.com>
2026-01-02 10:38:11 -0700

    build publish workshop

--- z_server_scripts/serverDZ_Tombstone_Diag_Server.bat
+++ z_server_scripts/serverDZ_Tombstone_Diag_Server.bat
@@ -11,9 +11,48 @@ echo (%time%) %serverName% started.
 
 dir
 
-start "DayZ Server Diag" /min "DayZDiag_x64.exe" -config=serverDZ_Tombstone.cfg -port=2302 "-profiles=profile_tombstonediag" -limitfps=120 "-mod=Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@CF;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@VPPAdminTools;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@VanillaPlusPlusMap;P:\Mods\Tombstone;P:\Mods\@MyTestMod;" -noPause -forcedebugger -scriptDebug=true -dologs -adminlogs -server -newErrorsAreWarnings=1 -scrAllowFileWrite -filePatching
+start "DayZ Server Diag" /min "DayZDiag_x64.exe" -config=serverDZ_Tombstone.cfg -port=2302 "-profiles=profile_tombstonediag" -limitfps=120 "-serverMod=P:\Mods\SRP_Server_Scripts;" "-mod=Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@CF;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@VPPAdminTools;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@VanillaPlusPlusMap;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@DayZ Horse;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Survivor Animations;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Code Lock;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals Slot Mod;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals Simple Snapping;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@IAT_Crafting_Plus;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\Ambient Animals Pack;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Survivalists_Core_Scripts;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Survivalists_WesternZ_Scripts;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Survivalists_Metallurgy;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Survivalists_Mining;P:\Mods\@ALV_UN_Core;P:\Mods\@ALV_UN_Clothing;P:\Mods\@ALV_UN_Gear;P:\Mods\@ALV_UN_NPC;P:\Mods\@ALV_UN_Props;P:\Mods\@ALV_UN_Scripts;P:\Mods\@ALV_UN_Structures;P:\Mods\@ALV_UN_Structures_Camp;P:\Mods\@ALV_UN_Structures_Commercial;P:\Mods\@ALV_UN_Structures_Industrial;P:\Mods\@ALV_UN_Structures_Misc;P:\Mods\@ALV_UN_Structures_Residential;P:\Mods\@ALV_UN_Structures_SignsDecals;P:\Mods\@ALV_UN_Structures_Walls;P:\Mods\@ALV_UN_Weapons;P:\Mods\@Tombstone;P:\Mods\@WesternZ_World_assets;" -noPause -forcedebugger -scriptDebug=true -dologs -adminlogs -server -newErrorsAreWarnings=1 -scrAllowFileWrite -filePatching
 
 
 
 
-@REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals Slot Mod;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Code Lock;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals Simple Snapping;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@DayZ Horse;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Survivor Animations;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@IAT_Crafting_Plus_Enums;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@IAT_Crafting_Plus;Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@WesternZ - Server Mod;P:\Mods\Tombstone;P:\Mods\@IAT_InfinityGauntlet_Release;
+
+@REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@CF;
+@REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@VPPAdminTools;
+@REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@VanillaPlusPlusMap;
+@REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@DayZ Horse;
+@REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Survivor Animations;
+@REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Code Lock
+@REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals Slot Mod;
+@REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Admirals Simple Snapping;
+@REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@IAT_Crafting_Plus;
+@REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\Ambient Animals Pack;
+@REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Survivalists_Core_Scripts;
+@REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Survivalists_WesternZ_Scripts;
+@REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Survivalists_Metallurgy;
+@REM Y:\SteamLibrary\steamapps\common\DayZ\!Workshop\@Survivalists_Mining;
+@REM P:\Mods\@ALV_UN_Core;
+@REM P:\Mods\@ALV_UN_Clothing;
+@REM P:\Mods\@ALV_UN_Gear;
+@REM P:\Mods\@ALV_UN_NPC;
+@REM P:\Mods\@ALV_UN_Props;
+@REM P:\Mods\@ALV_UN_Scripts;
+@REM P:\Mods\@ALV_UN_Structures;
+@REM P:\Mods\@ALV_UN_Structures_Camp;
+@REM P:\Mods\@ALV_UN_Structures_Commercial;
+@REM P:\Mods\@ALV_UN_Structures_Industrial;
+@REM P:\Mods\@ALV_UN_Structures_Misc;
+@REM P:\Mods\@ALV_UN_Structures_Residential;
+@REM P:\Mods\@ALV_UN_Structures_SignsDecals;
+@REM P:\Mods\@ALV_UN_Structures_Walls;
+@REM P:\Mods\@ALV_UN_Weapons;
+@REM P:\Mods\@Tombstone;
+@REM P:\Mods\@WesternZ_World_assets;
+
+
+
+
+
+
+@REM C:\Program Files (x86)\Steam\steamapps\common\DayZ\!Workshop\@Dabs Framework;
+@REM C:\Program Files (x86)\Steam\steamapps\common\DayZ\!Workshop\@Admirals Chat Filter;
