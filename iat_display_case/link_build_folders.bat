0f3935ed4392d73542bb31b350cc1bdf2e41f04b
Treee <itsatreee.twitch@gmail.com>
2026-01-12 12:38:38 -0700

    initial commit

---
+++ iat_display_case/link_build_folders.bat
@@ -0,0 +1,2 @@
+mklink /J "C:\Users\Tree\Documents\DayZ Projects\Mods\@IAT_DisplayCase_Dev" Z:\DayZ\Modding\iat_display_case\.hemttout\dev
+mklink /J "C:\Users\Tree\Documents\DayZ Projects\Mods\@IAT_DisplayCase_Release" Z:\DayZ\Modding\iat_display_case\.hemttout\release
