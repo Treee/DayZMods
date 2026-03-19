9e60c35cd7d0b7cdb7b3d33aeded27fd0d392f00
Treee <itsatreee.twitch@gmail.com>
2026-03-19 14:58:48 -0600

    initial commit

---
+++ iat_personal_tent/link_build_folders.bat
@@ -0,0 +1,2 @@
+mklink /J "C:\Users\Tree\Documents\DayZ Projects\Mods\@IAT_PersonalTent_Dev" Z:\DayZ\Modding\iat_personal_tent\.hemttout\dev
+mklink /J "C:\Users\Tree\Documents\DayZ Projects\Mods\@IAT_PersonalTent_Release" Z:\DayZ\Modding\iat_personal_tent\.hemttout\release
