92bbddcb2222699ebdf25c361c67821cb0e845fa
Treee <itsatreee.twitch@gmail.com>
2026-03-19 14:09:20 -0600

    removed commented code

--- iat_dayz_codex/addons/scripts/5_mission/mission/modded_missionserver.c
+++ iat_dayz_codex/addons/scripts/5_mission/mission/modded_missionserver.c
@@ -12,12 +12,4 @@ modded class MissionServer
 		m_Codex.PrettyPrint();
 	}
 
-	override void OnClientReadyEvent(PlayerIdentity identity, PlayerBase player)
-	{
-		super.OnClientReadyEvent(identity, player);
-
-		auto configParams = new Param1<IAT_DayZCodexConfig>(GetDayZGame().GetIATDayZCodexConfig());
-		PrintFormat("Sending IAT_DayZCodex Mod Config to Player: %1 RPC: %2", identity.GetName(), IAT_RPC_DayZCodex.CHECK_CONFIG);
-		g_Game.RPCSingleParam(player, IAT_RPC_DayZCodex.CHECK_CONFIG, configParams, true, identity);
-	}
 };
