9a85a2109f352dc66cf4c62b0e6c58b1f328b527
Treee <itsatreee.twitch@gmail.com>
2026-04-10 09:16:43 -0600

    adding 1.29 update changes

--- iat_facepaints/release_version.hpp
+++ iat_facepaints/release_version.hpp
@@ -1,4 +1,4 @@
 #define MAJOR 0
-#define MINOR 40
+#define MINOR 41
 #define PATCH 23
 #define BUILD 215
--- iat_more_doors/release_version.hpp
+++ iat_more_doors/release_version.hpp
@@ -1,4 +1,4 @@
 #define MAJOR 0
 #define MINOR 3
 #define PATCH 2
-#define BUILD 160
+#define BUILD 162
